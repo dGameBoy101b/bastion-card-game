4544586408e9eed18e85c88d4d953f8036ae033d
dGameBoy101b <rhysmader@gmail.com>
2025-01-30 15:35:08 +0800

    Created bastion game and bastion game view

---
+++ bastion_game.py
@@ -0,0 +1,50 @@
+from random import shuffle
+from typing import Callable
+from bastion_game_view import BastionGameView
+from battle import Battle
+from battle_decider import BattleDecider
+from rank import Rank
+from war_scorer import WarScorer
+
+class BastionGame:
+	def __init__(self, battle_decider: BattleDecider = BattleDecider(), war_scorer: WarScorer = WarScorer()):
+		self.battle_decider = battle_decider
+		self.war_scorer = war_scorer
+		self.player1_view = BastionGameView(self, True)
+		self.player2_view = BastionGameView(self, False)
+		self.reset()
+	
+	def reset(self) -> None:
+		self.player1_cards = set(Rank)
+		self.player2_cards = set(Rank)
+		neutrals = list(Rank)
+		shuffle(neutrals)
+		self.battles = tuple(Battle(neutral) for neutral in neutrals)
+		self.current_index = 0
+	
+	def player1_score(self) -> int:
+		self.war_scorer.player1_score(self.battles)
+	
+	def player2_score(self) -> int:
+		self.war_scorer.player2_score(self.battles)
+
+	def winner(self) -> Battle.Result:
+		return self.war_scorer.war_result(self.battles)
+
+	def play_next_turn(self, player1_strategy: Callable[[BastionGameView], Rank], player2_strategy: Callable[[BastionGameView], Rank]) -> None:
+		if self.current_index >= len(self.battles):
+			raise IndexError("No next turn to play")
+		
+		player1_card = player1_strategy(self.player1_view)
+		if player1_card not in self.player1_cards:
+			raise ValueError("Player 1 tried to play card they do not have")
+		
+		player2_card = player2_strategy(self.player2_view)
+		if player2_card not in self.player2_cards:
+			raise ValueError("Player 2 tried to play card they do not have")
+		
+		battle = self.battles[self.current_index]
+		battle.player1 = player1_card
+		battle.player2 = player2_card
+		self.battle_decider.determine_result(battle)
+		self.current_index += 1
---
+++ bastion_game_view.py
@@ -0,0 +1,42 @@
+from bastion_game import BastionGame
+from battle import Battle
+from battle_decider import BattleDecider
+from rank import Rank
+from war_scorer import WarScorer
+
+class BastionGameView:
+	def __init__(self, game:BastionGame, is_player1: bool = True):
+		self.__game = game
+		self.__is_player1 = is_player1
+	
+	def decider(self) -> BattleDecider:
+		return self.__game.battle_decider
+	
+	def scorer(self) -> WarScorer:
+		return self.__game.war_scorer
+
+	def your_cards(self) -> set[Rank]:
+		return self.__game.player1_cards if self.__is_player1 else self.__game.player2_cards
+	
+	def opponents_cards(self) -> set[Rank]:
+		return self.__game.player2_cards if self.__is_player1 else self.__game.player1_cards
+	
+	def your_score(self) -> int:
+		return self.__game.player1_score() if self.__is_player1 else self.__game.player2_score()
+	
+	def opponent_score(self) -> int:
+		return self.__game.player2_score() if self.__is_player1 else self.__game.player1_score()
+		
+	def current_neutral(self) -> Rank:
+		return self.__game.battles[self.__game.current_index].neutral
+	
+	def next_neutrals(self) -> tuple[Rank]:
+		return tuple(battle.neutral for battle in self.__game.battles[self.__game.current_index:])
+	
+	def previous_battles(self) -> tuple[Battle]:
+		'''You are player 1 in returned battles'''
+		battles = self.__game.battles[:self.__game.current_index]
+		if not self.__is_player1:
+			for battle in battles:
+				battle.flip_players()
+		return battles
