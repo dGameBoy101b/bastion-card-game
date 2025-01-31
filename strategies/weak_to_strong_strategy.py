1028fecca43c8bd3c809c2df38917fd59a5db459
dGameBoy101b <rhysmader@gmail.com>
2025-02-01 01:10:24 +0800

    Extracted weakest method.

--- strategies/weak_to_strong_strategy.py
+++ strategies/weak_to_strong_strategy.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+from battle_decider import BattleDecider
 from game_view import GameView
 from battle import Battle
 from rank import Rank
@@ -15,19 +17,22 @@ class WeakToStrongStrategy:
 		self.result_strength = WeakToStrongStrategy.DEFAULT_RESULT_STRENGTH
 		self.result_strength.update(result_strength)
 
-	def card_strength(self, card: Rank, view: GameView) -> int:
+	def card_strength(self, card: Rank, next_neutrals: Iterable[Rank], decider: BattleDecider) -> int:
 		strength = 0
-		for neutral in view.next_neutrals():
-			result = view.decider().determine_result(Battle(neutral, card, neutral))
+		for neutral in next_neutrals:
+			result = decider.determine_result(Battle(neutral, card, neutral))
 			strength += self.result_strength[result]
 		return strength
-
-	def __call__(self, view:GameView) -> Rank:
+	
+	def weakest(self, your_cards: Iterable[Rank], next_neutrals: Iterable[Rank], decider: BattleDecider) -> Rank:
 		weakest: Rank = None
 		weakest_strength: int = None
-		for card in view.your_cards():
-			strength = self.card_strength(card, view)
+		for card in your_cards:
+			strength = self.card_strength(card, next_neutrals, decider)
 			if weakest is None or strength < weakest_strength:
 				weakest = card
 				weakest_strength = strength
 		return weakest
+
+	def __call__(self, view:GameView) -> Rank:
+		return self.weakest(view.your_cards(), view.next_neutrals(), view.decider())
