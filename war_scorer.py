cb064d35e0c43ae6379220c2481f303cb195f166
dGameBoy101b <rhysmader@gmail.com>
2025-02-01 00:03:07 +0800

    Fixed scorer not flipping battles for player 2 score. Fixed scorer tests not using right constants.

--- war_scorer.py
+++ war_scorer.py
@@ -17,9 +17,7 @@ class WarScorer:
 		return score
 	
 	def player2_score(self, battles: Tuple[Battle]) -> int:
-		for battle in battles:
-			battle = battle.fliped_players()
-		return self.player1_score(battles)
+		return self.player1_score((battle.fliped_players() for battle in battles))
 	
 	def war_result(self, battles: Tuple[Battle]) -> Battle.Result | None:
 		if len(battles) < 1 or battles[-1].result is None:
@@ -59,5 +57,5 @@ if __name__ == '__main__':
 	assert scorer.player2_score(PLAYER1_TIE) == 0
 
 	PLAYER2_TIE = (Battle(Rank.Two, result=Battle.Result.Tie), Battle(Rank.Two, result=Battle.Result.Player2Win))
-	assert scorer.player1_score(PLAYER1_TIE) == 0
-	assert scorer.player2_score(PLAYER1_TIE) == 2
+	assert scorer.player1_score(PLAYER2_TIE) == 0
+	assert scorer.player2_score(PLAYER2_TIE) == 2
