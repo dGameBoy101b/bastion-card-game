054089fbcb6a56a82ba3b99080ac4368d3897bcc
dGameBoy101b <rhysmader@gmail.com>
2025-01-31 23:52:54 +0800

    Created scorer tests

--- war_scorer.py
+++ war_scorer.py
@@ -29,3 +29,35 @@ class WarScorer:
 		if player1_score == player2_score:
 			return Battle.Result.Tie
 		return Battle.Result.Player1Win if player1_score > player2_score else Battle.Result.Player2Win
+	
+if __name__ == '__main__':
+	from rank import Rank
+	scorer = WarScorer()
+
+	EMPTY = tuple()
+	assert scorer.player1_score(EMPTY) == 0
+	assert scorer.player2_score(EMPTY) == 0
+
+	NONE1 = (Battle(Rank.Two, result=None),)
+	assert scorer.player1_score(NONE1) == 0
+	assert scorer.player2_score(NONE1) == 0
+
+	TIE1 = (Battle(Rank.Two, result=Battle.Result.Tie),)
+	assert scorer.player1_score(TIE1) == 0
+	assert scorer.player2_score(TIE1) == 0
+
+	PLAYER1 = (Battle(Rank.Two, result=Battle.Result.Player1Win),)
+	assert scorer.player1_score(PLAYER1) == 1
+	assert scorer.player2_score(PLAYER1) == 0
+
+	PLAYER2 = (Battle(Rank.Two, result=Battle.Result.Player2Win),)
+	assert scorer.player1_score(PLAYER2) == 0
+	assert scorer.player2_score(PLAYER2) == 1
+
+	PLAYER1_TIE = (Battle(Rank.Two, result=Battle.Result.Tie), Battle(Rank.Two, result=Battle.Result.Player1Win))
+	assert scorer.player1_score(PLAYER1_TIE) == 2
+	assert scorer.player2_score(PLAYER1_TIE) == 0
+
+	PLAYER2_TIE = (Battle(Rank.Two, result=Battle.Result.Tie), Battle(Rank.Two, result=Battle.Result.Player2Win))
+	assert scorer.player1_score(PLAYER1_TIE) == 0
+	assert scorer.player2_score(PLAYER1_TIE) == 2
