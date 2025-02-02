a36b43d3fc0a1d04de28b93bdf42f0aa1ce57586
dGameBoy101b <rhysmader@gmail.com>
2025-02-02 22:15:33 +0800

    Extracted strategy register

---
+++ strategy_register.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import List
+import ask_multichoice
+from game import Strategy
+
+@dataclass(frozen=True)
+class StrategyInfo:
+	strategy: Strategy
+	name: str
+
+strategies: List[StrategyInfo] = []
+
+def ask_strategy(prompt: str) -> Strategy:
+	global strategies
+	choices = (info.name for info in strategies)
+	result = (info.strategy for info in strategies)
+	index = ask_multichoice(prompt, choices)
+	return result[index]
