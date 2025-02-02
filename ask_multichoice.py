7673f903d130d605648001e79869646ffe354e35
dGameBoy101b <rhysmader@gmail.com>
2025-02-02 22:24:25 +0800

    Added exception when no choices for multichoice. Fixed typo.

--- ask_multichoice.py
+++ ask_multichoice.py
@@ -2,6 +2,8 @@ from typing import Iterable
 
 def ask_multichoice(prompt: str, choices: Iterable[str], *, raise_exceptions: bool = False) -> int:
 	lines = [f"{index}: {choices[index]}" for index in range(len(choices))]
+	if len(lines) < 1:
+		raise ValueError("No choices to select")
 	prompt = f"{'\n'.join(lines)}\n{prompt}"
 	while True:
 		response = input(prompt)
@@ -19,7 +21,7 @@ def ask_multichoice(prompt: str, choices: Iterable[str], *, raise_exceptions: bo
 			continue
 		if response >= len(choices):
 			if raise_exceptions:
-				raise IndexError(f"{response} is not lessern than {len(choices)}")
+				raise IndexError(f"{response} is not lesser than {len(choices)}")
 			print(f"Enter a number lesser than {len(choices)}")
 			continue
 		return response
