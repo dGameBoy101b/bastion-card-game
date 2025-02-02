d34b6282d1aaae694970a5de655c326691d5be1f
dGameBoy101b <rhysmader@gmail.com>
2025-02-02 22:34:45 +0800

    Fixed ask multichoice not handling generators well.

--- ask_multichoice.py
+++ ask_multichoice.py
@@ -1,8 +1,12 @@
 from typing import Iterable
 
 def ask_multichoice(prompt: str, choices: Iterable[str], *, raise_exceptions: bool = False) -> int:
-	lines = [f"{index}: {choices[index]}" for index in range(len(choices))]
-	if len(lines) < 1:
+	index = 0
+	lines = []
+	for choice in choices:
+		lines.append(f"{index}: {choice}")
+		index += 1
+	if index < 1:
 		raise ValueError("No choices to select")
 	prompt = f"{'\n'.join(lines)}\n{prompt}"
 	while True:
@@ -19,9 +23,9 @@ def ask_multichoice(prompt: str, choices: Iterable[str], *, raise_exceptions: bo
 				raise IndexError(f"{response} is not 0 or greater")
 			print("Enter a number 0 or greater")
 			continue
-		if response >= len(choices):
+		if response >= index:
 			if raise_exceptions:
-				raise IndexError(f"{response} is not lesser than {len(choices)}")
-			print(f"Enter a number lesser than {len(choices)}")
+				raise IndexError(f"{response} is not lesser than {index}")
+			print(f"Enter a number lesser than {index}")
 			continue
 		return response
